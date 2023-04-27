8bb6c0917576bff424157b4b4cc0c367582d67f8
sasakurayanagi <kd1318241@st.kobedenshi.ac.jp>
2023-04-27 10:59:18 +0900

    project初回Commit
    初回Commitテスト

---
+++ BaseFrameworkHD2D/Src/Application/main.cpp
@@ -0,0 +1,376 @@
+﻿#include "main.h"
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// エントリーポイント
+// アプリケーションはこの関数から進行する
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+int WINAPI WinMain(HINSTANCE, HINSTANCE , LPSTR , int )
+{
+	// メモリリークを知らせる
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+	// COM初期化
+	if (FAILED(CoInitializeEx(nullptr, COINIT_MULTITHREADED)))
+	{
+		CoUninitialize();
+
+		return 0;
+	}
+
+	// mbstowcs_s関数で日本語対応にするために呼ぶ
+	setlocale(LC_ALL, "japanese");
+
+	//===================================================================
+	// 実行
+	//===================================================================
+	Application::Instance().Execute();
+
+	// COM解放
+	CoUninitialize();
+
+	return 0;
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション更新の前処理
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::PreUpdate()
+{
+	// 入力状況の更新
+	KdInputManager::Instance().Update();
+
+	KdShaderManager::Instance().WorkAmbientController().PreUpdate();
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション更新
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::Update()
+{
+	m_pos.y += 0.01f;
+	m_pos.z += 0.01f;
+	m_scaleMat = Math::Matrix::CreateScale(m_size.x, m_size.y, m_size.z);
+	m_roteMat = Math::Matrix::CreateRotationY(DirectX::XMConvertToRadians(m_deg));
+	m_transMat = Math::Matrix::CreateTranslation(m_pos.x, m_pos.y, m_pos.z);
+	m_mat = m_scaleMat * m_roteMat * m_transMat;//w=s*r*t
+
+	m_deg +=1;
+
+
+	//m_mat._43 = m_pos.z;
+	
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション更新の後処理
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::PostUpdate()
+{
+	// 3DSoundListnerの行列を更新
+	KdAudioManager::Instance().SetListnerMatrix(KdShaderManager::Instance().GetCameraCB().mView.Invert());
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション描画の前処理
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::PreDraw()
+{
+	KdDirect3D::Instance().ClearBackBuffer();
+
+	KdShaderManager::Instance().WorkAmbientController().PreDraw();
+
+	KdShaderManager::Instance().m_postProcessShader.PreDraw();
+
+
+	//カメラ
+	KdCamera gameCamera;//ローカル宣言、推奨
+	m_cameraTransMat = Math::Matrix::Identity;
+	//m_cameraMat = Math::Matrix::CreateTranslation(0, 4.0f, 0);//位置情報のみの行列
+	
+	m_cameraRoteMatX = Math::Matrix::Identity;
+	//m_cameraRotMat = Math::Matrix::CreateRotationX(DirectX::XMConvertToRadians(45));//回転情報のみの行列
+
+	m_worldCamMat = Math::Matrix::Identity;
+	//m_worldCamMat = m_cameraRotMat * m_cameraMat;//w=s*r*t
+
+	//カメラがキャラクターを追いかける
+	//キャラクターの行列を考慮する
+	
+	m_cameraTransMat = Math::Matrix::CreateTranslation(0, 4.0f, -5.0f);//位置情報のみの行列
+	m_cameraRoteMatX = Math::Matrix::CreateRotationX(DirectX::XMConvertToRadians(45));//X軸回転情報のみの行列
+	m_cameraRoteMatY = Math::Matrix::CreateRotationY(DirectX::XMConvertToRadians(0));//Y軸回転情報のみの行列
+
+	m_worldCamMat = (m_cameraRoteMatX * m_cameraTransMat * m_cameraRoteMatY) * (m_mat);//追従する行列グループ×追従される行列グループ
+	
+
+
+
+	gameCamera.SetCameraMatrix(m_worldCamMat);
+	gameCamera.SetToShader();
+
+
+
+	
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション描画
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::Draw()
+{
+
+	KdSquarePolygon* pCharactor = new KdSquarePolygon();//KdsquarePolygonClass
+	pCharactor->SetMaterial("Asset/Data/sample/sampleChara.png");
+
+	KdModelData* pTexture = new KdModelData;
+	pTexture->Load("Asset/Data/sample/Terrain/Terrain.gltf");
+
+
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 光を遮るオブジェクト(不透明な物体や2Dキャラ)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_HD2DShader.BeginGenerateDepthMapFromLight();
+	{
+		
+	}
+	KdShaderManager::Instance().m_HD2DShader.EndGenerateDepthMapFromLight();
+
+
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 陰影のあるオブジェクト(不透明な物体や2Dキャラ)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_HD2DShader.BeginLit();
+	{
+
+		//地形描画
+		KdShaderManager::Instance().m_HD2DShader.DrawModel(*pTexture);
+
+		//ポリゴン描画
+		//Math::Matrix m_mat = Math::Matrix::Identity;
+		//m_mat = Math::Matrix::CreateTranslation(m_pos.x, m_pos.y, m_pos.z);
+		KdShaderManager::Instance().m_HD2DShader.DrawPolygon(*pCharactor, m_mat);
+		
+	}
+	KdShaderManager::Instance().m_HD2DShader.EndLit();
+	
+	
+	KdSafeDelete(pCharactor);
+
+	KdSafeDelete(pTexture);
+
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 陰影のないオブジェクト(透明な部分を含む物体やエフェクト)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_HD2DShader.BeginUnLit();
+	{
+
+	}
+	KdShaderManager::Instance().m_HD2DShader.EndUnLit();
+
+
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 光源オブジェクト(自ら光るオブジェクトやエフェクト)はBeginとEndの間にまとめてDrawする
+	KdShaderManager::Instance().m_postProcessShader.BeginBright();
+	{
+
+	}
+	KdShaderManager::Instance().m_postProcessShader.EndBright();
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション描画の後処理
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::PostDraw()
+{
+	// 画面のぼかしや被写界深度処理の実施
+	KdShaderManager::Instance().m_postProcessShader.PostEffectProcess();
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// 2Dスプライトの描画
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::DrawSprite()
+{
+	// ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+	// 2Dの描画はこの間で行う
+	KdShaderManager::Instance().m_spriteShader.Begin();
+	{
+
+	}
+	KdShaderManager::Instance().m_spriteShader.End();
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション初期設定
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+bool Application::Init(int w, int h)
+{
+	//===================================================================
+	// ウィンドウ作成
+	//===================================================================
+	if (m_window.Create(w, h, "3D GameProgramming", "Window") == false) {
+		MessageBoxA(nullptr, "ウィンドウ作成に失敗", "エラー", MB_OK);
+		return false;
+	}
+
+	//===================================================================
+	// フルスクリーン確認
+	//===================================================================
+	bool bFullScreen = false;
+	/*if (MessageBoxA(m_window.GetWndHandle(), "フルスクリーンにしますか？", "確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES) {
+		bFullScreen = true;
+	}*/
+
+	//===================================================================
+	// Direct3D初期化
+	//===================================================================
+
+	// デバイスのデバッグモードを有効にする
+	bool deviceDebugMode = false;
+#ifdef _DEBUG
+	deviceDebugMode = true;
+#endif
+
+	// Direct3D初期化
+	std::string errorMsg;
+	if (KdDirect3D::Instance().Init(m_window.GetWndHandle(), w, h, deviceDebugMode, errorMsg) == false) {
+		MessageBoxA(m_window.GetWndHandle(), errorMsg.c_str(), "Direct3D初期化失敗", MB_OK | MB_ICONSTOP);
+		return false;
+	}
+
+	// フルスクリーン設定
+	if (bFullScreen) {
+		KdDirect3D::Instance().WorkSwapChain()->SetFullscreenState(TRUE, 0);
+	}
+
+	//===================================================================
+	// シェーダー初期化
+	//===================================================================
+	KdShaderManager::Instance().Init();
+
+	KdAudioManager::Instance().Init();
+
+	m_mat = Math::Matrix::Identity;
+	m_transMat = Math::Matrix::Identity;
+	m_roteMat = Math::Matrix::Identity;
+
+
+	m_pos = { 0,0,1.0f };
+
+	m_size = { 1.0f,1.0f,1.0f };
+
+	m_deg = 0;
+
+	return true;
+}
+
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+// アプリケーション実行
+// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// ///// /////
+void Application::Execute()
+{
+	KdCSVData windowData("Asset/Data/WindowSettings.csv");
+	const std::vector<std::string>& sizeData = windowData.GetLine(0);
+
+	//===================================================================
+	// 初期設定(ウィンドウ作成、Direct3D初期化など)
+	//===================================================================
+	if (Application::Instance().Init(atoi(sizeData[0].c_str()), atoi(sizeData[1].c_str())) == false) {
+		return;
+	}
+
+	//===================================================================
+	// ゲームループ
+	//===================================================================
+
+	// 時間
+	m_fpsController.Init();
+
+	// ループ
+	while (1)
+	{
+		// 処理開始時間Get
+		m_fpsController.UpdateStartTime();
+
+		// ゲーム終了指定があるときはループ終了
+		if (m_endFlag)
+		{
+			break;
+		}
+
+		//=========================================
+		//
+		// ウィンドウ関係の処理
+		//
+		//=========================================
+
+		// ウィンドウのメッセージを処理する
+		m_window.ProcessMessage();
+
+		// ウィンドウが破棄されてるならループ終了
+		if (m_window.IsCreated() == false)
+		{
+			break;
+		}
+
+		if (GetAsyncKeyState(VK_ESCAPE))
+		{
+//			if (MessageBoxA(m_window.GetWndHandle(), "本当にゲームを終了しますか？",
+//				"終了確認", MB_YESNO | MB_ICONQUESTION | MB_DEFBUTTON2) == IDYES)
+			{
+				End();
+			}
+		}
+		//=========================================
+		//
+		// アプリケーション更新処理
+		//
+		//=========================================
+		PreUpdate();
+
+		Update();
+
+		PostUpdate();
+
+		//=========================================
+		//
+		// アプリケーション描画処理
+		//
+		//=========================================
+		PreDraw();
+
+		Draw();
+
+		PostDraw();
+
+		DrawSprite();
+
+		// BackBuffer -> 画面表示
+		KdDirect3D::Instance().WorkSwapChain()->Present(0, 0);
+
+		//=========================================
+		//
+		// フレームレート制御
+		//
+		//=========================================
+
+		m_fpsController.Update();
+	}
+
+	//===================================================================
+	// アプリケーション解放
+	//===================================================================
+	Release();
+}
+
+// アプリケーション終了
+void Application::Release()
+{
+	KdInputManager::Instance().Release();
+
+	KdShaderManager::Instance().Release();
+
+	KdAudioManager::Instance().Release();
+
+	KdDirect3D::Instance().Release();
+
+	// ウィンドウ削除
+	m_window.Release();
+}
---
+++ BaseFrameworkHD2D/Src/Application/main.h
@@ -0,0 +1,80 @@
+﻿#pragma once
+
+//============================================================
+// アプリケーションクラス
+//	APP.～ でどこからでもアクセス可能
+//============================================================
+class Application
+{
+// メンバ
+public:
+
+	// アプリケーション実行
+	void Execute();
+
+	// アプリケーション終了
+	void End() { m_endFlag = true; }
+
+	HWND GetWindowHandle() const { return m_window.GetWndHandle(); }
+	int GetMouseWheelValue() const { return m_window.GetMouseWheelVal(); }
+
+	int GetNowFPS() const { return m_fpsController.m_nowfps; }
+	int GetMaxFPS() const { return m_fpsController.m_maxFps; }
+
+private:
+
+	void PreUpdate();
+	void Update();
+	void PostUpdate();
+
+	void PreDraw();
+	void Draw();
+	void PostDraw();
+
+	void DrawSprite();
+
+	// アプリケーション初期化
+	bool Init(int w, int h);
+
+	// アプリケーション解放
+	void Release();
+
+	// ゲームウィンドウクラス
+	KdWindow		m_window;
+
+	// FPSコントローラー
+	KdFPSController	m_fpsController;
+
+	// ゲーム終了フラグ trueで終了する
+	bool		m_endFlag = false;
+
+	Math::Matrix m_mat;
+	Math::Matrix m_transMat;
+	Math::Matrix m_scaleMat;
+	Math::Matrix m_roteMat;
+	Math::Vector3 m_pos;
+	Math::Vector3 m_size;
+
+
+
+	Math::Matrix m_worldCamMat;
+	Math::Matrix m_cameraTransMat;
+	Math::Matrix m_cameraRoteMatX;
+	Math::Matrix m_cameraRoteMatY;
+	Math::Vector3 m_cameraPos;
+
+	int m_deg;
+
+//=====================================================
+// シングルトンパターン
+//=====================================================
+private:
+	// 
+	Application() {}
+
+public:
+	static Application &Instance(){
+		static Application Instance;
+		return Instance;
+	}
+};
